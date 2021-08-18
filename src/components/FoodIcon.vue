8f715a682226d619fece818fa4f5ce6291c7b882
LayMui <mui_ume@hotmail.com>
2021-08-18 19:58:59 +0800

    Add props

---
+++ src/components/FoodIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="food-icon"></div>
+  <div class="food-name">
+  <p>{{name}}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'FoodIcon',
+  props: { name: String }
+};
+    
+</script>
+
+<style scoped>
+.food-icon {
+    display: flex;
+    align-items: flex-end;
+    width: 800px;
+    height: 700px;
+    border: 1px solid grey;
+    background-image: url('../assets/chestnuts.png')
+}
+
+.food-name {
+    color: white;
+    text-align: center;
+    background-color: rgba(0,0,0,0.5);
+    width: 100%
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,17 +2,33 @@
   <div class=food-homepage-container>
   <h1>The Food App</h1>
   <p>Find out all about different foods and put together your favourite meal here!</p>
+  <div class=food-items>
+  <FoodIcon :name='foods[3].name'/>
+  </div>
   </div>
 </template>
 
 <script>
+
+import FoodIcon from './FoodIcon.vue';
+
 export default {
     name: 'HomePage', 
     data: function() {
         return {
             title: 'The Food App',
-            description: 'Find out all about different food and put together your fav. meals' 
+            description: 'Find out all about different food and put together your fav. meals',
+             foods: [
+            { name: 'apple', description: 'red and crunchy', src: '../assets/apple.png', percent_carbs: 96, is_fav:false},
+            { name: 'banana', description: 'yellow and soft', src: '../assets/banana.png', percent_carbs: 93, is_fav:true},
+            { name: 'orange', description: 'sweet and juicy', src: '../assets/orange.png', percent_carbs: 53, is_fav:true},
+            { name: 'chestnut', description: 'delightful soft', src: '../assets/chestnuts.png', percent_carbs: 72, is_fav:true},
+        ]
+           
         }
+    },
+    components: {
+        FoodIcon
     }
 };
 </script>
