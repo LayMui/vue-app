809808641ba665413f87a6418086b03ed7cfdf35
LayMui <mui_ume@hotmail.com>
2021-08-18 18:45:51 +0800

    Initial Commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=food-homepage-container>
+  <h1>The Food App</h1>
+  <p>Find out all about different foods and put together your favourite meal here!</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage', 
+    data: function() {
+        return {
+            title: 'The Food App',
+            description: 'Find out all about different food and put together your fav. meals' 
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
